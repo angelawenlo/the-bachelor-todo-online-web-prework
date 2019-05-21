311f0ec5cc9c858208036c9f54c6274608100f9f
Angela Lo <angelawenlo@gmail.com>
2019-05-21 20:02:52 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -66,7 +66,7 @@ def get_average_age_for_season(data, seasons)
       #contestants_info = individual contestant info
       if season == seasons
         age = contestants_info["age"].to_i
-        
+        age.inject {|sum, n| sum + n}
 
         binding.pry
         return
