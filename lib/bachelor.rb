958fe97c9219288c3e7f082356ab49652ac96d41
Angela Lo <angelawenlo@gmail.com>
2019-05-21 17:37:40 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,6 @@ def get_first_name_of_season_winner(data, seasons)
     # code here
   data.each do |season, contestants|
   contestants.each do |contestants_info|
-          binding.pry
       if season == seasons
         return contestants_info["name"].split(" ")[0]
       end
