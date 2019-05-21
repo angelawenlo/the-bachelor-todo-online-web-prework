85d5a73038ddc2c499b7c27c7c9247382862d87f
Angela Lo <angelawenlo@gmail.com>
2019-05-21 18:08:33 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -35,7 +35,6 @@ def count_contestants_by_hometown(data, hometown)
       #contestants_info = individual contestant info
   if contestants_info["hometown"] == hometown
       counter = counter + 1
-      binding.pry
   end
   counter
 end
