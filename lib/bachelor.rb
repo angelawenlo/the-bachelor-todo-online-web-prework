0ffdd2b1f4ece647aac37083c55ba091965bff2e
Angela Lo <angelawenlo@gmail.com>
2019-05-21 16:54:53 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def get_first_name_of_season_winner(data, seasons)
   data.each do |season, contestants|
-  contestants.each do |contestants_info, data|
+  contestants.each do |contestants_info|
 binding.pry
   # code here
     end
