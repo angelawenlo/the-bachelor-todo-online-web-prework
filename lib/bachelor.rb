0d3d5665a56503e3ec746bdb2d5287f540f36fe3
Angela Lo <angelawenlo@gmail.com>
2019-05-21 17:38:03 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,10 +13,10 @@ end
 def get_contestant_name(data, occupation)
   # code here
   data.each do |season, contestants|
-  contestants.each do |contestants_info|
+    contestants.each do |contestants_info|
     binding.pry
+    end
   end
-end
 end
 #def count_contestants_by_hometown(data, hometown)
   # code here
