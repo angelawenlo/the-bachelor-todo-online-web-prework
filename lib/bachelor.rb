c79ff35026b9576056ff5f10a7f6495abcde66f3
Angela Lo <angelawenlo@gmail.com>
2019-05-21 17:36:25 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -11,9 +11,9 @@ def get_first_name_of_season_winner(data, seasons)
   end
 end
 
-#def get_contestant_name(data, occupation)
+def get_contestant_name(data, occupation)
   # code here
-#end
+end
 
 #def count_contestants_by_hometown(data, hometown)
   # code here
